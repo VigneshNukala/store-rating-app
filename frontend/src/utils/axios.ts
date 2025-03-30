628822054143ebb6a4ea78f0002fca7c975acc30
Vignesh Nukala <125763653+VigneshNukala@users.noreply.github.com>
2025-03-30 15:12:46 -0700

    Update axios.ts

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: `${API_BASE_URL`},
   withCredentials: true,
 });
 
